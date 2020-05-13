6e326c959241d9448bd376f607c8b8a1b4a0fc55
NothAmor <i@nothamor.cn>
2020-05-13 22:35:06 +0800

    Update files

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$server = "localhost";
-$username = "root";
-$password = "root";
-$db_name = "question";
-
-$con = new mysqli($server, $username, $password, $db_name);
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['login']);
-session_destroy();
-
-echo "<script>alert('注销成功');window.location.href='index.php';</script>";
